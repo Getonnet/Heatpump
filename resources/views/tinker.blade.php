50b505eab90bb89ddad5b6b189ebd514eaa00a35
hassantvs24 <hassantvs24@gmail.com>
2020-12-09 14:10:15 +0600

    kindly Intigration

--- resources/views/tinker.blade.php
+++ resources/views/tinker.blade.php
@@ -5,26 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hit-Pump</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/chat.min.css')}}">
 </head>
 <body>
+
+
+{{--<script>--}}
+{{--    var botmanWidget = {--}}
+{{--        aboutText: 'bot',--}}
+{{--        introMessage: `Welcome!--}}
+{{--If you want a good offer on air to air heat pump which is the most common solution in detached houses, then it is me you should "talk" to!--}}
+{{--If you want a solution for air to water or water to liquid, click HERE and we will contact you. (Same contact info as requested on the next page)--}}
+{{--I am Heating and Cooling Systems' digital heat pump consultant. I am here to give you good advice and help you through the process of ordering the right heat pump for your house and your purpose.--}}
+{{--I can also give you 1000 kroner in discount in relation to what our "human sellers" can do.--}}
+{{--When ordering, you can choose to pay by card, installment or request an invoice.--}}
+{{--Once you have ordered, you will be contacted by one of our certified fitters who will ask some control questions and arrange a time for installation.--}}
+{{--Safe and easy!--}}
+{{--For mobile user; I recommend you bring your tablet or PC so you can see photos and other information Before I can give you advice and guidance, I would like to get to know you a little. First: refers to the support text.`--}}
+{{--    };--}}
+{{--</script>--}}
+
+<script id="kindly-chat" src="https://chat.kindlycdn.com/kindly-chat.js" data-bot-key="e77a739f-9ac2-4707-8c4c-30ae6b77ed4b" data-shadow-dom async></script>
+
+{{--<script src='{{asset('js/widget.js')}}'></script>--}}
 </body>
-
-<link rel="stylesheet" type="text/css" href="{{asset('css/chat.min.css')}}">
-<script>
-    var botmanWidget = {
-        aboutText: 'bot',
-        introMessage: `Welcome!
-If you want a good offer on air to air heat pump which is the most common solution in detached houses, then it is me you should "talk" to!
-If you want a solution for air to water or water to liquid, click HERE and we will contact you. (Same contact info as requested on the next page)
-I am Heating and Cooling Systems' digital heat pump consultant. I am here to give you good advice and help you through the process of ordering the right heat pump for your house and your purpose.
-I can also give you 1000 kroner in discount in relation to what our "human sellers" can do.
-When ordering, you can choose to pay by card, installment or request an invoice.
-Once you have ordered, you will be contacted by one of our certified fitters who will ask some control questions and arrange a time for installation.
-Safe and easy!
-For mobile user; I recommend you bring your tablet or PC so you can see photos and other information Before I can give you advice and guidance, I would like to get to know you a little. First: refers to the support text.`
-    };
-</script>
-
-<script src='{{asset('js/widget.js')}}'></script>
-
 </html>
